77593287e33fff4b96d17820214cde43c0b4ba35
Logan Perkins <logan@github.lp-programming.com>
2024-11-02 22:28:57 -0700

    Tangle

--- fake_decompile.py
+++ fake_decompile.py
@@ -17,11 +17,37 @@ def fake_decompile(func, seekdef = False):
         print(18, start, end)
         return str.join("", lines[start:end])
     else: # Do our best to reconstruct a suitable signature, this should work passably even for lambdas
-        # TODO:  Finish reconstructing signature
         names = func.__code__.co_varnames
         argcount = func.__code__.co_argcount
         kwonly = func.__code__.co_kwonlyargcount
-        
-        
-        signature = f"def {func.__name__}():\n"
+        posonly = func.__code__.co_posonlyargcount
+        defaults = func.__defaults__
+        kwdefaults = func.__kwdefaults__
+        varargs = func.__code__.co_flags & 4
+        kwvarargs = func.__code__.co_flags & 8
+        args = []
+        kw = []
+        for i in range(argcount):
+            args.append(names[i])
+        for i in range(kwonly):
+            kw.append(names[i+argcount])
+        if defaults:
+            for i in reversed(defaults):
+                args[-1 -i] += f"={repr(i)}"
+        for idx, i in enumerate(kw):
+            if i in kwdefaults:
+               kw[idx] += f"={repr(kwdefaults[i])}"
+        if varargs:
+            va = names[argcount+kwonly]
+            args.append(f"*{va}")
+        if kwvarargs:
+            kwa = names[argcount+kwonly+bool(varargs)]
+            kw.append(f"**{kwa}")
+        if posonly:
+            args.insert(posonly, "/")
+        if kwonly and not varargs:
+            args.append("*")
+
+        args.extend(kw)
+        signature = f"def {func.__name__}({str.join(', ', args)}):\n"
         return str.join("", [signature, *lines[start:end]])
